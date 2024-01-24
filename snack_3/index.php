6db88dcc1b393b0e2e456c208ede433a0ce754e2
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-24 17:53:25 +0100

    Fine Snack 3

--- snack_3/index.php
+++ snack_3/index.php
@@ -41,3 +41,33 @@
 ?>
 
 <!-- LOGICA DI VISUALIZZAZIONE -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Snack 3</title>
+</head>
+<body>
+    <h1 class="text-center my-4">Ultime notizie</h1>
+    <div class="container">
+        <div class="row">
+            <?php foreach($posts as $index => $day){ ?>
+                <div class="row">
+                    <h3><?php echo $index; ?></h3>
+                    <?php foreach($day as $post){ ?>
+                        <div class="">                
+                            <h5> <?php echo $post['title']; ?> </h5>
+                            <h6> <?php echo $post['author']; ?> </h6>
+                            <p> <?php echo $post['text']; ?> </p>
+                        </div> 
+                    <?php } ?>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</body>
+</html>
