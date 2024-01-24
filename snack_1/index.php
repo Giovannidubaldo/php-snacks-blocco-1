494276c5509c43689cfbe87fe37b334894dd7045
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-24 15:56:05 +0100

    Created array on snack 1

---
+++ snack_1/index.php
@@ -0,0 +1,48 @@
+<!-- LOGICA DI LAVORO -->
+
+<?php 
+    echo '<pre>';
+    $match = [
+        [
+            'casa' => 'Indiana Pacers',
+            'trasferta' => 'Denver Nuggets',
+            'score_casa' => '109',
+            'score_trasferta' => '114',
+        ],
+        [
+            'casa' => 'Brooklyne Nets',
+            'trasferta' => 'New Yorks Knicks',
+            'score_casa' => '103',
+            'score_trasferta' => '108',
+        ],
+        [
+            'casa' => 'Dallas Mavericks',
+            'trasferta' => 'Boston Celtics',
+            'score_casa' => '110',
+            'score_trasferta' => '119',
+        ],
+        [
+            'casa' => 'Sacramento Kings',
+            'trasferta' => 'Atlanta Hawks',
+            'score_casa' => '122',
+            'score_trasferta' => '107',
+        ]
+    ];
+
+    var_dump($match)
+?>
+
+<!-- LOGICA DI VISUALIZZAZIONE -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Snack 1</title>
+</head>
+<body>
+    
+</body>
+</html>
